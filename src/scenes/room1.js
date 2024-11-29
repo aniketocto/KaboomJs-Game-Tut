4332771c9840ac506544454abfe9804ad59d50e0
aniketocto <aniketocto@gmail.com>
2024-11-29 19:00:00 +0530

    first commit

---
+++ src/scenes/room1.js
@@ -0,0 +1,21 @@
+import { setBackgroundColor } from "./room.utils.js";
+
+export function room1(k, roomData) {
+    setBackgroundColor(k, '#a2aed5')
+
+    const roomLayers = roomData.layers;
+
+    const map = k.add([
+        k.pos(0, 0),
+        k.sprite("room1")
+    ])
+
+    const colliders = [];
+    for (const layer of roomLayers) {
+        if (layer.name === "colliders") {
+            colliders.push(...layer.objects)
+            break;
+        }
+    }
+
+}
